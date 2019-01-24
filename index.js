b727abb327c8cca08b47ba1a65706cab7b1bcbf7
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-24 20:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function doToElementsInArray(array, callback) {
 
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+  console.log("----", array[index])
 }
 
 var animals = ["dog", "cat", "squirrel"];
